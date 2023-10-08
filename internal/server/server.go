6cf93ee26e5d75d60274d4017dc9e15a0cb1c4fb
Hamza Rahmani <mrezio450@gmail.com>
2023-10-07 21:08:22 -0400

    Partially implement server logic. Test currently hangs due to the nil handler.

---
+++ internal/server/server.go
@@ -0,0 +1,41 @@
+package server
+
+import (
+	"context"
+	"net"
+	"net/http"
+	"time"
+)
+
+type HTTPServer struct {
+	server *http.Server
+}
+
+// TODO: Create a handler, otherwise test hangs
+func NewHTTPServer() *HTTPServer {
+	return &HTTPServer{
+		&http.Server{
+			Addr:              "0.0.0.0:4000",
+			Handler:           nil,
+			ReadHeaderTimeout: 3 * time.Second,
+		},
+	}
+}
+
+func (h *HTTPServer) Start() error {
+	l, err := net.Listen("tcp4", h.server.Addr)
+	if err != nil {
+		return err
+	}
+	err = h.server.Serve(l)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func (h *HTTPServer) Stop() error {
+	ctx := context.Background()
+	err := h.server.Shutdown(ctx)
+	return err
+}
