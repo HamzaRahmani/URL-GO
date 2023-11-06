87c1cbd5eb78cacab33c097c2c912c0c1059e450
Hamza Rahmani <mrezio450@gmail.com>
2023-11-05 19:41:38 -0500

    very much lost

--- internal/server/server_test.go
+++ internal/server/server_test.go
@@ -1,8 +1,11 @@
 package server_test
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/HamzaRahmani/urlShortner/internal/server"
@@ -39,4 +42,37 @@ func TestStopServer(t *testing.T) {
 
 	// Assert
 	assert.NotNil(t, err)
+}
+
+// TODO: Write test for handler, use chi and create an endpoint for create URL
+// TODO: Fix this test, the chi router should be modular... maybe get familiar with chi and then progress with writing test
+func TestCreateShortURL(t *testing.T) {
+	// Arrange
+	port, _ := tests.GetFreeTCPPort(t)
+	handler = routeHandler{}
+
+	srv := server.NewHTTPServer(port, handler)
+	go func() { _ = srv.Start() }()
+	defer func() { _ = srv.Stop() }()
+	tests.WaitUntilBusyPort(string(rune(port)), t)
+
+	json, _ := json.Marshal(requestBody{
+		Page: "http://www.google.com",
+	})
+	body := bytes.NewReader(json)
+
+	// Act
+	req := httptest.NewRequest(http.MethodPost, fmt.Sprintf("http://127.0.0.1:%d/short-url", port), body)
+	w := httptest.NewRecorder()
+
+	// res, err := http.Post(fmt.Sprintf("", port))
+	defer func() { _ = res.Body.Close() }()
+
+	// Assert
+	assert.NoError(t, err)
+
+}
+
+type requestBody struct {
+	Page string `json:"page"`
 }
