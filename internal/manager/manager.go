4cc993c37115cc905f3c7f67f2c097e7e0998d1c
Hamza Rahmani <mrezio450@gmail.com>
2023-12-31 16:40:01 -0500

    Add failing test case for maanger

--- internal/manager/manager.go
+++ internal/manager/manager.go
@@ -1,10 +1,20 @@
 package manager
 
+import "github.com/HamzaRahmani/urlShortner/internal/database"
+
 type Manager interface {
 	CreateURL(message string) (string, error)
 }
 
 type manager struct {
-	// database Database
+	database database.Database
 	// analyzer Analyzer
+}
+
+func NewManager(dB database.Database) *manager {
+	return &manager{database: dB}
+}
+
+func (m *manager) CreateURL(text string) (string, error) {
+	return "", nil
 }
