715f7933a539e525a21950e64d336ee94927b940
Hamza Rahmani <mrezio450@gmail.com>
2024-01-02 20:19:32 -0500

    Update end to end test to use ephemeral database

--- e2e/e2e_test.go
+++ e2e/e2e_test.go
@@ -7,8 +7,8 @@ import (
 	"testing"
 
 	"github.com/gavv/httpexpect/v2"
+	"github.com/jackc/pgx/v5/pgxpool"
 
-	"github.com/HamzaRahmani/urlShortner/internal/config"
 	"github.com/HamzaRahmani/urlShortner/internal/database"
 	"github.com/HamzaRahmani/urlShortner/internal/manager"
 	"github.com/HamzaRahmani/urlShortner/internal/server"
@@ -20,10 +20,28 @@ type requestBody struct {
 	Expire bool   `json:"expiry"`
 }
 
+var testDbInstance *pgxpool.Pool
+var testDbAddress string
+
+func TestMain(m *testing.M) {
+	testDB := tests.SetupTestDatabase()
+	testDbInstance = testDB.DbInstance
+	testDbAddress = testDB.DbAddress
+	defer testDB.TearDown()
+	os.Exit(m.Run())
+}
+
 func TestCreateURL(t *testing.T) {
 	port, _ := tests.GetFreeTCPPort(t)
 	// TODO: use ephemeral database used in database_test.go
-	db, err := database.NewPostgresStore(CreateConnString())
+	db, err := database.NewPostgresStore(
+		fmt.Sprintf("postgres://%s:%s@%s/%s?sslmode=disable",
+			tests.DbUser,
+			tests.DbPass,
+			testDbAddress,
+			tests.DbName),
+	)
+
 	if err != nil {
 		panic(err)
 	}
@@ -46,15 +64,15 @@ func TestCreateURL(t *testing.T) {
 		JSON().Object().ContainsKey("shortURL")
 }
 
-func CreateConnString() string {
-	c := config.Init(os.Environ())
-	user, _ := c.GetDatabaseUser()
-	pass, _ := c.GetDatabasePassword()
-	host, _ := c.GetDatabaseHost()
+// func CreateConnString() string {
+// 	c := config.Init(os.Environ())
+// 	user, _ := c.GetDatabaseUser()
+// 	pass, _ := c.GetDatabasePassword()
+// 	host, _ := c.GetDatabaseHost()
 
-	connString := fmt.Sprintf("postgresql://%s:%s@%s?sslmode=disable", user, pass, host)
-	return connString
-}
+// 	connString := fmt.Sprintf("postgresql://%s:%s@%s?sslmode=disable", user, pass, host)
+// 	return connString
+// }
 
 // func TestGetURL(t *testing.T) {
 // 	// Arrange
