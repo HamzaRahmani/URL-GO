aa83574a2f2633b65ed23a7d899027c35bb447e9
Hamza Rahmani <mrezio450@gmail.com>
2024-01-12 20:03:04 -0500

    Utilize useState for simplicity

--- frontend/src/components/URLForm.tsx
+++ frontend/src/components/URLForm.tsx
@@ -2,25 +2,51 @@ import { type FormEvent, useState } from "react";
 
 export default function Form() {
   const [responseMessage, setResponseMessage] = useState("");
+  const [url, setUrl] = useState("");
 
-  async function submit(e: FormEvent<HTMLFormElement>) {
+  function submit(e: FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    const formData = new FormData(e.target as HTMLFormElement);
-    const response = await fetch("/api/feedback", {
+
+    fetch("http://localhost:5050/url", {
       method: "POST",
-      body: formData,
-    });
-    const data = await response.json();
-    if (data.message) {
-      setResponseMessage(data.message);
-    }
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        url: url,
+      }),
+    })
+      .then(async (response) => {
+        if (response.ok) {
+          const data = await response.json();
+          console.log(data);
+          if (data.message) {
+            setResponseMessage(data.message);
+          }
+          return;
+        }
+
+        throw new Error("Something went wrong, try again later");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setUrl(event.target.value);
+  };
 
   return (
     <form onSubmit={submit}>
       <label>Enter a URL</label>
-      <input type="url" id="url" name="url" autoComplete="url" required />
-
+      <input
+        onChange={handleChange}
+        value={url}
+        type="url"
+        id="url"
+        name="url"
+        autoComplete="url"
+        required
+      />
       <button>Send</button>
       {responseMessage && <p>{responseMessage}</p>}
     </form>
