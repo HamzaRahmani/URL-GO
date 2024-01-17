e848ecc8ab36132336b65541bf41335cec902268
Hamza Rahmani <mrezio450@gmail.com>
2024-01-16 19:26:20 -0500

    try using vercel for deployment

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,8 +1,8 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import node from "@astrojs/node";
-
 import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,11 +14,5 @@ export default defineConfig({
     },
   },
   output: "hybrid",
-  adapter: node({
-    mode: "standalone",
-  }),
-  server: {
-    host: "0.0.0.0",
-    port: 4000,
-  },
+  adapter: vercel(),
 });
